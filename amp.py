985b1bc4e54784f0592a78f7dde756de8c8d7b63
Nicholas Young <ndy14@ufl.edu>
2019-01-25 00:04:24 -0500

    Now properly parses all data in feeds.json, gets proper AMP urls for all valid URLS (should also properly wait if the AMP API is exhausted), sends AMP urls to HTML parser, and gets back summaries. Note that the HTML parser only currently has functions for CNN and huffington post, so only those websites could have summaries returned. Also, the RSS URLs for huffington post and foxnews are not working.

--- amp.py
+++ amp.py
@@ -1,9 +1,60 @@
 import requests
 
-def ampify_url(url):
-    data = { 'urls':[url,] }
+def ampify_url_list(article_url_list):
+    """ Returns a mapping of input URLs to AMP Cache URLs
+
+    Uses the Google AMP API to return a dict of AMP Cache URLs for valid news article URLs.
+
+    Args:
+        article_url_list: A list of URLS to articles
+
+    Returns: 
+        A dict mapping the valid input URLs to their AMP URLs.
+        E.g. { 'url1':'amp_url1', 'url2':'amp_url2', ... }
+
+    Raises:
+        Exception: Quota reached - wait a few seconds before attempting to access the API again.
+        requests.exceptions.RequestException
+    """
+    data = { 'urls':article_url_list }
     params = { 'key':'AIzaSyAuOS_oCZCG6s70iDjjcV2IOW1qM0yd2sc' }
+
     resp = requests.post('https://acceleratedmobilepageurl.googleapis.com/v1/ampUrls:batchGet', params=params, json=data)
 
     resp_json = resp.json()
-    return resp_json['ampUrls'][0]['cdnAmpUrl'].strip()
+    # print (resp_json)
+
+    # Check if quota reached
+    try:
+        print ('ERROR: RESPONSE CODE ' + resp_json['error']['code'] + ': ' + resp_json['error']['message'])
+        raise Exception("Quota reached - wait a few seconds before attempting to access the API again.")
+    except KeyError as ke:
+        pass
+
+    amp_dict = {}
+    invalid_urls = []
+    try:
+        for AmpUrl in resp_json['ampUrls']:
+            amp_dict[AmpUrl['originalUrl']] = AmpUrl['cdnAmpUrl']
+    except KeyError as ke:
+        print ("No ampUrls in API response")
+
+    try:
+        for AmpUrlError in resp_json['urlErrors']:
+            errorCode = AmpUrlError['errorCode']
+            errorMessage = AmpUrlError['errorMessage']
+            originalUrl = AmpUrlError['originalUrl']
+
+            print ('Encountered error "' + errorCode + ': ' + errorMessage)
+            print ('Skipping URL... ' + originalUrl)
+
+            invalid_urls.append(originalUrl)
+
+        print ("Skipped the following URLS: ")
+        print (invalid_urls)
+    except KeyError as ke:
+        print ("No urlErrors in API response")
+    
+    # print ("amp dict:")
+    # print (amp_dict)
+    return amp_dict
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 import json
 import requests
+import time
 import xml.etree.ElementTree as ET
-from amp import ampify_url
+from amp import ampify_url_list
 
 def loadJson():
-	with open('cnn.json') as f:
+	with open('feeds.json') as f:
 		data = json.load(f)
 
 	return data["top_stories"]
@@ -14,8 +15,11 @@ def parseFeed(feed):
 	domain = feed['domain']
 	print('--------------------------------%s---------------------------------' %(domain))
 
+
+	# Retrieve the RSS page for the given feed.
+	# Max 20 attempts before printing an error and returning.
 	curAttempts = 0
-	maxAttempts = 20
+	MAX_ATTEMPTS = 20
 	while True:
 		try:
 			resp = requests.get(feed['rss_url'])
@@ -23,80 +27,142 @@ def parseFeed(feed):
 		except requests.exceptions.ConnectionError as err: # Connection refused --> abort
 			print('############################ CONNECTION ERROR #############################') # The current link for foxnews appears to be down
 			print(err)
-			return
+			raise
 		except requests.exceptions.ContentDecodingError as err:
-			if curAttempts > maxAttempts:
+			if curAttempts > MAX_ATTEMPTS:
 				print('############################ DECODING ERROR #############################') # huffingtonpost will sometimes throw this error
 				print(err)
-				return
+				raise
 
 			curAttempts += 1
 			continue
 
+	# Parse the RSS file as XML.
+	# Max 20 attempts before printing an error and returning.
 	curAttempts = 0
 	while True:
 		try:
 			tree = ET.fromstring(resp.text)
 			break
 		except ET.ParseError as err:
-			if curAttempts > maxAttempts:
+			if curAttempts > MAX_ATTEMPTS:
 				print('############################ PARSING ERROR #############################') # huffingtonpost will sometimes throw this error
 				print(err)
-				return
+				raise
 
 			curAttempts += 1
 			continue
-	
 
-	articlePayload = []
+	article_list = []
 	for item in tree.findall('./channel/item'):
 		ad = False # We do not keep any items labeled as ads
+		error_encountered = False # Only keep items which do not encounter errors during this process
 
 		for child in item:
-			if(child.tag == 'rss_url'):
-				link = child.text
-				if(domain not in child.text):
+			# Get item's article url
+			if(child.tag == 'link'):
+				article_url = child.text
+				try:
+					resp = requests.get(article_url)
+					article_url = resp.url
+				except requests.exceptions.ConnectionError as err:
+					print("ERROR GETTING URL: " + article_url + " IN DOMAIN " + domain)
+					print (err)
+					error_encountered = True
+					break
+
+				# Check if this article is an ad
+				if( ("www." + domain not in article_url) and ("http://" + domain not in article_url) and (domain + ".com" not in article_url) and (domain + ".org" not in article_url) ):
 					ad = True
 					break
 
-			if(child.tag == 'link'):
-				url = child.text
-				try:
-					r = requests.get(url)
-					url = r.url
-				except:
-					print("ERROR GETTING URL")
-					print(url)
-
+			# Get article description
 			if(child.tag == 'description'):
 				description = child.text
 
+			# Get article title
 			if(child.tag == 'title'):
 				title = child.text
 
-		if (ad == False):
-			articlePayload.append({'domain': domain, 'title': title, 'description': description, 'url': url})
+		if (ad == False and error_encountered == False):
+			article_entry = { 'domain': domain, 'title': title, 'description': description, 'article_url': article_url, 'amp_url':'', 'summary':'' }
+			article_list.append(article_entry)
 
-	return articlePayload
+	return article_list
 
 def main():
-	feeds = loadJson()
+	feed_list = loadJson()
+	big_list = []
+	for entry in feed_list:
+		try:
+			big_list.append(parseFeed(entry))
+		except requests.exceptions.ConnectionError:
+			continue
+		except requests.exceptions.ContentDecodingError:
+			continue
+		except ET.ParseError:
+			continue
 
-	allFeeds = []
+	# Get all valid article URLs from all news sources
+	url_list = []
+	for small_list in big_list:
+		for item in small_list:
+			url_list.append(item['article_url'])
 
-	for feed in feeds:
-		allFeeds.append(parseFeed(feed))
-	
-	for feedList in allFeeds:
-		print("Processing: %s" %(feedList[0].get('domain')))
-		for article in feedList:
-			requests.post(url = "http://jist-html-parser-container/parse", data = article)
+	# Fetch AMP urls for every article
+	amp_dict_list = []
+	for i in range (0, len(url_list), 50):
+		while True:
+			try:
+				amp_dict = ampify_url_list(url_list[i:i+50])
+			except Exception as e:
+				print (e)
+				# Wait some time before retrying
+				time.sleep(5)
+				print ("Waiting ...")
+				continue
+			break
 
-	# TEST PAYLOAD
-	#payload = {'url': 'https://amp-cnn-com.cdn.ampproject.org/c/s/amp.cnn.com/cnn/2019/01/23/politics/donald-trump-nancy-pelosi-government-shutdown-congress/index.html', 'domain': 'cnn'}
-	#requests.post(url = "http://jist-html-parser-container/parse", data = payload) 
+		amp_dict_list.append(amp_dict)
 
+	# Update all entries with their amp urls
+	for amp_dict in amp_dict_list:
+		for key, value in amp_dict.items():
+			found_entry = False
+			# Find the entry in big_list with the same article_url as this item
+			for small_list in big_list:
+				for entry in small_list:
+					if entry['article_url'] == key:
+						entry['amp_url'] = value
+						found_entry = True
+						break
 
+				if (found_entry == True):
+					break
+
+	for small_list in big_list:
+		for item in small_list:
+			# Skip items with empty AMP urls
+			if item['amp_url'] == '':
+				continue
+			
+			# Send POST for getting summary for article
+			data = { 'url':item['amp_url'], 'domain':item['domain'] }
+			resp = requests.post(url='http://jist-html-parser-container/parse', json=data)
+
+			print ('Response <' + str(resp.status_code) + '>')
+
+			# Try to get summary
+			try:
+				summary = resp.json()['summary']
+			except json.decoder.JSONDecodeError as jde_error: # URL was probably invalid or weird
+				item['amp_url'] = '' # Mark the article as to be discarded and continue
+				continue
+
+			item['summary'] = summary
+
+			print ('Article ' + item['title'] + ': ')
+			print (summary)
 
 if __name__ == "__main__":
 	main()
